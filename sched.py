246c03f4c310c826327bc01738f1983eea97f95e
Vitali Sarayeu <vitali_sarayeu@epam.com>
2016-03-30 16:59:50 +0300

    Set schedule to run every day at 21.05

--- sched.py
+++ sched.py
@@ -5,12 +5,9 @@ import upload
 def action():
     upload.main()
 
-schedule.every(1).minutes.do(action)
+# schedule.every(1).minutes.do(action)
+schedule.every().day.at("21:05").do(action)
 
-# Log to file
-# logfile = open("schedule.log", "w")
-# logfile.write("Starting scheduled run...\n")
-# logfile.close()
 upload.write_log("Starting scheduled run...\n")
 
 while True:
